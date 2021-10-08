6c65e48900feab78be1bc1a3881cd9020e46a3a6
Dharmaraj777 <87357702+Dharmaraj777@users.noreply.github.com>
2021-10-08 17:41:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  8 17:28:29 2021
-
-@author: 91779
-"""
-
 mkdir -p ~/.streamlit/
 echo "
-[general]n
-email = "your-email@domain.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+[server]\n
+headless = true\n
+enableCORS=false\n
+port = $PORT\n
+\n
 " > ~/.streamlit/config.toml
